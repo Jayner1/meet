b4e161f76e25cc3e15e0bc5c8cc90cb5a5b1080d
Jayner1 <106836096+Jayner1@users.noreply.github.com>
2022-12-28 09:46:23 -0800

    commas

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -37,13 +37,7 @@ const oAuth2Client = new google.auth.OAuth2(
  *
  */
 module.exports.getAuthURL = async () => {
-  /**
-   *
-   * Scopes array passed to the `scope` option. Any scopes passed must be enabled in the
-   * "OAuth consent screen" settings in your project on your Google Console. Also, any passed
-   *  scopes are the ones users will see when the consent screen is displayed to them.
-   *
-   */
+
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: "offline",
     scope: SCOPES,
@@ -52,7 +46,7 @@ module.exports.getAuthURL = async () => {
   return {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*'
+      'Access-Control-Allow-Origin': '*',
     },
     body: JSON.stringify({
       authUrl: authUrl,
@@ -88,7 +82,7 @@ module.exports.getAccessToken = async (event) => {
         return {
           statusCode: 200,
           headers: {
-            'Access-Control-Allow-Origin': '*'
+            'Access-Control-Allow-Origin': '*',
           },
           body: JSON.stringify(token),
         };
@@ -99,7 +93,7 @@ module.exports.getAccessToken = async (event) => {
         return {
           statusCode: 500,
           headers: {
-            'Access-Control-Allow-Origin': '*'
+            'Access-Control-Allow-Origin': '*',
           },
           body: JSON.stringify(err),
         };
