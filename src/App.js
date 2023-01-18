c5ae3199ddf4a7dfeff78f5f5dbd31a280ed8930
Jayner1 <106836096+Jayner1@users.noreply.github.com>
2023-01-18 14:47:59 -0800

    chart

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { getEvents, extractLocations, checkToken, getAccessToken } from './api';
 import './nprogress.css';
-import WelcomeScreen from './WelcomeScreen';
+// import WelcomeScreen from './WelcomeScreen';
+import {
+  ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip
+} from 'recharts';
 
 class App extends Component {
   state = {
@@ -65,16 +68,32 @@ this.setState({ events, locations: extractLocations(events) });
   }
 
   render() {
-    if (this.state.showWelcomeScreen === undefined) return <div
-className="App" />
+    const { locations, numberOfEvents } = this.state;
     return (
       <div className="App">
-        {!navigator.onLine && (<h1>You are in offline mode!</h1>)}
-        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
+        <h1>Meet App</h1>
+        <h4>Choose your nearest city</h4>
+        <CitySearch updateEvents={this.updateEvents} locations={locations} />
+        <NumberOfEvents
+          updateEvents={this.updateEvents}
+          numberOfEvents={numberOfEvents}
+        />       
+         <h4>Events in each city</h4>
+
+         <ScatterChart
+          width={400}
+          height={400}
+          margin={{
+            top: 20, right: 20, bottom: 20, left: 20,
+          }}
+        >
+          <CartesianGrid />
+          <XAxis type="category" dataKey="city" name="city" />
+          <YAxis type="number" dataKey="number" name="number of events" />
+          <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+          <Scatter data={this.getData()} fill="#8884d8" />
+        </ScatterChart>
         <EventList events={this.state.events} />
-        <NumberOfEvents />
-        <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}
-    getAccessToken={() => { getAccessToken() }} />
       </div>
     );
   }
