fc437313e834e0a9b2a32034921b715081651e7f
Jayner1 <106836096+Jayner1@users.noreply.github.com>
2023-01-20 11:00:41 -0800

    test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,3 @@
-// import React, { Component } from 'react';
-// import './App.css';
-// import EventList from './EventList';
-// import EventGenre from './EventGenre';
-// import CitySearch from './CitySearch';
-// import NumberOfEvents from './NumberOfEvents';
-// import { getEvents, extractLocations, checkToken } from './api';
-// import './nprogress.css';
-// import WelcomeScreen from './WelcomeScreen';
-// import {
-//   ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer
-// } from 'recharts';
-
 import React, {Component} from 'react';
 import './App.css';
 import EventList from './EventList';
@@ -22,31 +9,33 @@ import EventGenre from './EventGenre';
 import { getEvents, extractLocations, checkToken, getAccessToken } from'./api';
 import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
+
 class App extends Component {
   state = {
     events: [],
     locations: [],
     numberOfEvents: 32,
+    offlineText: '',
     showWelcomeScreen: undefined
   }
 
-  updateEvents = (location, eventCount) => {
-    if (eventCount === undefined) {
-      eventCount = this.state.numberOfEvents;
-    } else this.setState({ numberOfEvents: eventCount });
-
+  updateEvents = (location) => {
     getEvents().then((events) => {
-      const locationEvents =
-        location === 'all'
-          ? events
-          : events.filter((event) => event.location === location);
-      const filteredEvents = locationEvents.slice(0, eventCount);
+      const locationEvents = 
+      (location === "all")
+      ? events
+      : events.filter((event) => event.location === location);
       this.setState({
-        events: filteredEvents,
-        currentLocation: location,
+        events: locationEvents.slice(0, this.state.numberOfEvents)
       });
     });
-  };
+  }
+
+  updateNumberOfEvents(number) {
+    this.setState({
+      numberOfEvents: number,
+    });
+  }
 
   getData = () => {
     const {locations, events} = this.state;
@@ -87,8 +76,8 @@ class App extends Component {
       }
 
   }
-  
-  componentWillUnmount(){
+
+  componentWillUnmount() {
     this.mounted = false;
   }
 
@@ -134,6 +123,91 @@ class App extends Component {
 }
 
 export default App;
+
+
+// import React, { Component } from 'react';
+// import './App.css';
+// import EventList from './EventList';
+// import EventGenre from './EventGenre';
+// import CitySearch from './CitySearch';
+// import NumberOfEvents from './NumberOfEvents';
+// import { getEvents, extractLocations, checkToken } from './api';
+// import './nprogress.css';
+// import WelcomeScreen from './WelcomeScreen';
+// import {
+//   ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer
+// } from 'recharts';
+
+// class App extends Component {
+//   state = {
+//     events: [],
+//     locations: [],
+//     numberOfEvents: 32,
+//     showWelcomeScreen: undefined
+//   }
+
+//   updateEvents = (location, eventCount) => {
+//     if (eventCount === undefined) {
+//       eventCount = this.state.numberOfEvents;
+//     } else this.setState({ numberOfEvents: eventCount });
+
+//     getEvents().then((events) => {
+//       const locationEvents =
+//         location === 'all'
+//           ? events
+//           : events.filter((event) => event.location === location);
+//       const filteredEvents = locationEvents.slice(0, eventCount);
+//       this.setState({
+//         events: filteredEvents,
+//         currentLocation: location,
+//       });
+//     });
+//   };
+
+//   getData = () => {
+//     const {locations, events} = this.state;
+//     const data = locations.map((location)=>{
+//       const number = events.filter((event) => event.location === location).length
+//       const city = location.split(', ').shift()
+//       return {city, number};
+//     })
+//     return data;
+//   };
+
+//   async componentDidMount() {
+//     this.mounted = true;
+//     const accessToken = localStorage.getItem('access_token');
+//     const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+//     const searchParams = new URLSearchParams(window.location.search);
+//     const code = searchParams.get("code");
+//     this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+//     if ((code || isTokenValid) && this.mounted) {
+//       getEvents().then((events) => {
+//         if (this.mounted) {
+//           this.setState({
+//             events: events.slice(0, this.state.numberOfEvents),
+//             locations: extractLocations(events)});
+//         }
+//       });
+//     }
+    
+//     if (!navigator.onLine) {
+//       this.setState({
+//         offlineText:
+//           "No internet connection. Data loaded from the cache.",
+//         });
+//       } else {
+//         this.setState({
+//           offlineText: '',
+//         });
+//       }
+
+//   }
+  
+//   componentWillUnmount(){
+//     this.mounted = false;
+//   }
+
 //   render() {
 //     const { locations, numberOfEvents, events } = this.state;
 //     return (
